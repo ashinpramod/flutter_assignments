ebc88d18ec92428b4c37e6e0b82d1dba26e438c7
ashin000 <ashinpramod010@gmail.com>
2023-12-29 23:17:52 +0530

    initial commit

---
+++ lib/Sharedpreferences/shared_Login.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_assignments/Sharedpreferences/shared_home.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'Shared_SignUp.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SharedLogin(),
+  ));
+}
+
+class SharedLogin extends StatefulWidget {
+  @override
+  State<SharedLogin> createState() => _SharedLoginState();
+}
+
+class _SharedLoginState extends State<SharedLogin> {
+  final uname_ctrl = TextEditingController();
+  final pwd_ctrl = TextEditingController();
+  late bool loggedin;
+  bool showpwd=true;
+  @override
+  void initState() {
+    check_user_loggedin();
+    super.initState();
+  }
+
+  void check_user_loggedin() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance()!;
+    /// if first statement is null execute second statement
+    loggedin = preferences.getBool("loggedin") ?? false;
+    if (loggedin == true) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => Home()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              "Login",
+              style: GoogleFonts.lato(
+                fontSize: 50,
+                color: Colors.red,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              "Login with your credentials",
+              style: GoogleFonts.lato(
+                fontSize: 20,
+                color: Colors.black,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: uname_ctrl,style: GoogleFonts.lato(
+              fontSize: 20,
+              color: Colors.black,
+              fontStyle: FontStyle.italic,),
+
+              decoration: InputDecoration(
+                  hintText: "user name", border: OutlineInputBorder()),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: pwd_ctrl,style: GoogleFonts.lato(
+              fontSize: 20,
+              color: Colors.black,
+              fontStyle: FontStyle.italic,),
+              obscureText: showpwd,
+              obscuringCharacter: '*',
+              decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (showpwd) {
+                            showpwd = false;
+                          } else {
+                            showpwd = true;
+                          }
+                        });
+                      },
+                      icon: Icon(showpwd == true
+                          ? Icons.visibility
+                          : Icons.visibility_off_sharp)),
+                  border: OutlineInputBorder(),
+                  hintText: "Password"),),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: ()
+                {
+                  validate_login();
+
+
+                },
+                child: Text(
+                  "Login",
+                  style: GoogleFonts.lato(
+                    fontSize: 20,
+                    color: Colors.white60,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                style: const ButtonStyle(
+                  backgroundColor:
+                  MaterialStatePropertyAll<Color>(Colors.red),
+                )),
+          ),
+
+          const SizedBox(
+            height: 10,
+          ),
+
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => SharedSignup()));
+            },
+            child:  Text("Dont have an account? Register Here",style: GoogleFonts.lato(
+              fontSize: 20,
+              color: Colors.black,
+              fontStyle: FontStyle.italic,
+            ),),),
+
+        ]),
+      ),
+    );
+  }
+
+  void validate_login()async {
+
+    final SharedPreferences preferences = await SharedPreferences.getInstance()!;
+    String username = uname_ctrl.text;
+    String password = pwd_ctrl.text;
+    String? uname=preferences.getString("uname");
+    String? paswd=preferences.getString("password");
+    if (username == uname && password == paswd) {
+      //preferences.setString("uname", username);
+      preferences.setBool("loggedin", true);
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => Home()));
+    }
+
+  }
+
+
+}
---
+++ lib/Sharedpreferences/shared_home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_assignments/Sharedpreferences/shared_Login.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late String name;
+  late SharedPreferences preferences;
+
+  @override
+  void initState() {
+    fetch_data();
+    super.initState();
+  }
+
+  void fetch_data() async {
+    preferences = await SharedPreferences.getInstance();
+    setState(() {
+      name = preferences.getString("name")!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text(
+          "Hello $name",
+          style: GoogleFonts.lato(
+            fontSize: 28,
+            color: Colors.black,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        actions: [IconButton(onPressed: () async{
+          final SharedPreferences preferences = await SharedPreferences.getInstance()!;
+          preferences.setBool("loggedin", false);
+          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>SharedLogin()));
+        }, icon: Icon(Icons.power_settings_new_rounded),color: Colors.black,)],
+      ),
+    );
+  }
+}
